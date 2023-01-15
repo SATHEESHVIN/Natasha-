cdbcdf7ee1c04dc1802cc1e24f36d2fb686999dd
Vinarasan55 <122718267+Vinarasan55@users.noreply.github.com>
2023-01-15 15:13:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/DRAGON-PRO/FLIMS-HD-2.git /FLIMS-HD-2
+  git clone https://github.com/Vinarasan55/SKY-FILTER-HD.git /SKY-FILTER-HD
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /FLIMS-HD-2
+  git clone $UPSTREAM_REPO /SKY-FILTER-HD
 fi
-cd /FLIMS-HD-2
+cd /SKY-FILTER-HD
 pip3 install -U -r requirements.txt
 echo "🍃Starting Bot🍃...."
 python3 bot.py
